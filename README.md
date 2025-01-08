c8a05c24f6a2b4dcfeaa8ba2c71b2a349a372f84
Alberto Mengali <disberd@gmail.com>
2025-01-08 12:29:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestAllocations.jl
+
+This package simply provides a macro to test for allocations based on Chairmarks.jl. It tries to solve the potential problems of false allocations that can occur when using the `@allocated` or `@allocations` macros (see e.g. [this zulip link](https://julialang.zulipchat.com/#narrow/stream/225542-helpdesk/topic/.E2.9C.94.20Testing.20allocations/near/290507860) or [this discourse post](https://discourse.julialang.org/t/is-it-possible-to-add-reliable-tests-that-functions-do-not-allocate/85320/4)).
---
+++ src/TestAllocations.jl
@@ -0,0 +1,66 @@
+module TestAllocations
+
+
+using Chairmarks: Chairmarks
+using Test
+
+export @check_allocations
+
+function extract_args(expr)
+    Meta.isexpr(expr, :call) || error("The first argument to this macro must be a function call")
+    op, func, limit = if expr.args[1] in (:(==), :(!=), :(<), :(<=), :(>), :(>=), :(===), :(!==))
+        # We have an explicit test provided
+        expr.args
+    else
+        # We have only the function, so we assume op is == and limit is 0
+        :(==), expr, 0
+    end
+    (; op, func, limit)
+end
+
+function process_func!(func_expr)
+    for (i, arg) in enumerate(func_expr.args[2:end])
+        if Meta.isexpr(arg, :parameters)
+            for (ki, kwarg) in enumerate(arg.args)
+                arg[ki] = process_kwarg(kwarg)
+            end
+        else
+            args[i] = process_arg(arg)
+        end
+    end
+end
+
+function process_arg(arg)
+    Meta.isexpr(arg, :kw) && error("Only pass keyword arguments after the `;` within the @check_allocations macro")
+    if arg isa Union{Expr, Symbol}
+        Expr(:$, arg)
+    else
+        arg
+    end
+end
+
+function process_kwarg(kwarg)
+    if kwarg isa Symbol
+        Expr(:kw, kwarg, process_arg(kwarg))
+    elseif Meta.isexpr(kwarg, :kw)
+        Expr(:kw, kwarg.args[1], process_arg(kwarg.args[2]))
+    else
+        error("Invalid keyword argument: $kwarg")
+    end
+end
+
+macro check_allocations(expr)
+    (; op, func, limit) = extract_args(expr)
+    if limit isa Expr
+        limit = esc(limit)
+    end
+    passed = Expr(:call, op, :allocations, limit)
+    be_expr = Chairmarks.process_args((func, :(evals = 1), :(samples = 0)))
+    :(let
+        b = $be_expr
+        allocations = first(b.samples).allocs
+        $passed
+    end)
+end
+
+end # module TestAllocations
